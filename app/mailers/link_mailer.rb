6bfdb0bcab08bade2e3e71e048466164d3ba4bc4
F3PiX <f3pixnl@gmail.com>
2017-04-13 20:28:49 +0200

    Declutter send_link method
    Remove redundant instance var

--- app/mailers/link_mailer.rb
+++ app/mailers/link_mailer.rb
@@ -1,12 +1,11 @@
 class LinkMailer < ApplicationMailer
 
   def send_link(sterrenlink)
-    @sterrenlink = sterrenlink
-    @applicant  = @sterrenlink.link_request.researcher
-    @send_to    = @sterrenlink.link_request.researcher_email
-    @oz         = @sterrenlink.link_request.research_proposal
-    @pv         = @sterrenlink.link_request.patient_org
-    @link_for_pv= @sterrenlink.output_link
+    @applicant  = sterrenlink.link_request.researcher
+    @send_to    = sterrenlink.link_request.researcher_email
+    @oz         = sterrenlink.link_request.research_proposal
+    @pv         = sterrenlink.link_request.patient_org
+    @link_for_pv= sterrenlink.output_link
 
     mail(to: @send_to, subject: "Re: Sterrenlink")
   end
