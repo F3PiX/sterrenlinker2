77f07302e390ddf460c0abadde8f8ed0e996b7dd
F3PiX <f3pixnl@gmail.com>
2017-04-20 13:07:31 +0200

    Move article prefixer method from Sterrenlink to LinkRequest model
    The name_with_article is needed not alone to compose the Sterrenlink, but also in the mailer. So, it is now an instance method in the LinkRequest model.

--- app/mailers/link_mailer.rb
+++ app/mailers/link_mailer.rb
@@ -5,8 +5,9 @@ class LinkMailer < ApplicationMailer
     @applicant  = request.researcher
     @send_to    = request.researcher_email
     @oz         = request.research_proposal
-    @pv         = request.patient_org
+    @pv         = request.patient_org_with_article
     @link_for_pv= sterrenlink.output_link
-    mail(to: @send_to, subject: "Re: Sterrenlink")
+
+    mail(to: @send_to, subject: "Re: Sterrenlink", bcc: ENV['EMAIL'])
   end
 end
