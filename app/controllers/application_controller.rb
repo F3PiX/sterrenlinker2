72be78513e7a34c653a57b23250bfcedd250fef9
F3PiX <f3pixnl@gmail.com>
2017-07-08 13:51:05 +0200

    CHeck nr 5 RubyMine settings on newline :wrench:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #check for endline via VIM
   #check again after comp restart
+  # check with the settings from BigApple == exactly the opposite...
 end
